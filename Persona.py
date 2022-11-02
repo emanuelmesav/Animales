e3acce8aa81189f1bb98b65366e2b3d9c75241c3
Emanuel Mesa <esmesav@correo.udistrital.edu.co>
2022-11-02 17:19:43 -0500

    V1

---
+++ Persona.py
@@ -0,0 +1,37 @@
+
+
+class Persona:
+    def __init__(self):
+     self.nombre=""
+     self.edad=""
+     self.genero=""
+     self.volumen=""
+     self.bigote=""
+    
+    def saltar(self):
+        pass
+    def bailar(self):
+        pass
+    def gritar(self):
+        pass
+sujetoa=Persona()
+sujetoa.nombre="Camilo"
+sujetoa.edad=20
+sujetoa.genero="Pansexual"
+sujetoa.volumen="Alto"
+sujetoa.bigote=False
+
+sujetob=Persona()
+sujetob.nombre="Cristian"
+sujetoa.edad=20
+sujetoa.genero="Masculino"
+sujetoa.volumen="Medio"
+sujetoa.bigote=False
+
+sujetoc=Persona()
+sujetoc.nombre="Lina"
+sujetoc.edad=19
+sujetoc.genero="Femenino"
+sujetoc.volumen="Bajo"
+sujetoc.bigote=False
+
